baa0fd891098db0f8b93ece2a823ca08d0542148
qahwaa <qahwaabrand@gmail.com>
2025-04-30 02:00:27 +0300

    Add files via upload

--- cart.js
+++ cart.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // حساب التكلفة الإجمالية
     function calculateTotal() {
-        let total = cart.reduce((sum, item) => sum + (400 * item.quantity), 60);
+        let total = cart.reduce((sum, item) => sum + (450 * item.quantity), 50);
         totalCostElement.textContent = total + " LE";
     }
 
@@ -118,7 +118,7 @@ document.addEventListener("DOMContentLoaded", function () {
             .then(response => response.json())
             .then(data => {
                 if (data.ok) {
-                    alert("✅ تم إرسال الطلب بنجاح إلى البوت!");
+                    alert("✅ order sent successfully!");
                     localStorage.removeItem("cart");
                     cart = [];
                     renderCart();
